83ce4cd1b479bce1ebe7a742ac7b1b93d714e801
adamki <adajensen@gmail.com>
2016-02-03 21:10:06 -0700

    extracted ajax calls for all update and quality changes to a seperate method.

--- app/assets/javascripts/quality.js
+++ app/assets/javascripts/quality.js
@@ -10,64 +10,26 @@ var lowerQualities = {
   swill: "swill"
 }
 
+var editQuality = function(action){
+  $("i#" + action + "-quality").on('click', function(event){
+    var $idea      = $(this).closest('.idea');
+    var $quality   = $idea.attr('data-quality');
+    var $id        = $idea.attr('data-id');
+    var params = {
+      idea: {
+        quality: eval(action + 'Qualities[$quality]')
+      } 
+    }
+    sendAjax(params, $id, $idea)
+  })
+}
+
 function qualityListener(){
-  lowerIdeaQuality();
-  raiseIdeaQuality();
+  editQuality("raise");
+  editQuality("lower");
 }
 
-function raiseIdeaQuality(){
-  $("i#raise-quality").on('click', function(event){
-    var $idea      = $(this).closest('.idea');
-    var $quality   = $idea.attr('data-quality');
-    var $id        = $idea.attr('data-id');
-    
-    var params = {
-      idea: {
-        quality: raiseQualities[$quality]
-      } 
-    }
-
-    $.ajax({
-      type: 'PUT',
-      url: '/api/v1/ideas/' + $id,
-      data: params,
-      success: function(idea){
-        updateQuality($idea, idea.quality);
-      },
-      error: function(xhr){
-        console.log(xhr.responseText);
-      }
-    })
-  })
-}
-
-function lowerIdeaQuality(){
-  $("i#lower-quality").on('click', function(event){
-    var $idea      = $(this).closest('.idea');
-    var $quality   = $idea.attr('data-quality');
-    var $id        = $idea.attr('data-id');
-    
-    var params = {
-      idea: {
-        quality: lowerQualities[$quality]
-      } 
-    }
-
-    $.ajax({
-      type: 'PUT',
-      url: '/api/v1/ideas/' + $id,
-      data: params,
-      success: function(idea){
-        updateQuality($idea, idea.quality);
-      },
-      error: function(xhr){
-        console.log(xhr.responseText);
-      }
-    })
-  })
-}
-
-function updateQuality(idea, quality){
-  $(idea).find('.quality-description').html(quality)
-  $(idea).attr('data-quality', quality)
+function updateQuality(idea, $idea){
+  $idea.find('.quality-description').html(idea.quality)
+  $idea.attr('data-quality', idea.quality)
 }
